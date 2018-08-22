f03aee363ee364bea6f159421c9e9e641511213c
sronikle <sronikle@gmail.com>
2018-08-21 23:57:56 -0400

    test 06

--- public/main.js
+++ public/main.js
@@ -42,7 +42,7 @@ function test(){
      const data = { movie: choice };
 
      fetch('https://moviepasspoll.herokuapp.com/poll', {
-       method: 'post',
+       method: 'POST',
        body: JSON.stringify(data),
        headers: new Headers({
          'Content-Type': 'application/json'
@@ -66,79 +66,77 @@ function test(){
        });
   });
 
-
-  //Get the data from database
-  fetch('https://moviepasspoll.herokuapp.com/poll')
-    .then(function(res){ return res.json()})
-    .then(function(data){
-      const votesAll = data.votes;
-      //get the movies that have votes
-      //and creates array with all ID : votes
-
-      const voteCounts = votesAll.reduce(function(acc, vote){
-        acc[vote.movie] = (acc[vote.movie] || 0) + vote.votes
-        return acc;
-      }, {});
+  $.get('https://moviepasspoll.herokuapp.com/poll', function(data, status){
+    const votesAll = data.votes;
+    //get the movies that have votes
+    //and creates array with all ID : votes
+    const voteCounts = votesAll.reduce(function(acc, vote){
+      acc[vote.movie] = (acc[vote.movie] || 0) + vote.votes
+      return acc;
+    }, {});
 
 
-      //Add in the the information from the database if the movie has votes
-      $.each(dataPoints, function(key, val){
-        //val is the id for the database
-        $.each(voteCounts, function(i, value){
-          if ( val.id == parseInt(i)){
-            val.y = value;
-          }
-        });
+    //Add in the the information from the database if the movie has votes
+    $.each(dataPoints, function(key, val){
+      //val is the id for the database
+      $.each(voteCounts, function(i, value){
+        if ( val.id == parseInt(i)){
+          val.y = value;
+        }
       });
+    });
 
-      //Create chart in document
-      const chartCon = $('#chartContainer');
-      if (chartCon) {
-        var chart = new CanvasJS.Chart("chartContainer", {
-           animationEnabled: true,
-            theme: "theme1",
-            title: {
-                text: "Top 10 Choices"
-             },
-            axisY: {
-                title: "Units",
-                titleFontSize: 24
-              },
-             data: [{
-               type: "column",
-               dataPoints: dataPoints
-             }]
-          });
-        chart.render();
+    //Create chart in document
+    const chartCon = $('#chartContainer');
+    if (chartCon) {
+      var chart = new CanvasJS.Chart("chartContainer", {
+         animationEnabled: true,
+          theme: "theme1",
+          title: {
+              text: "Top 10 Choices"
+           },
+          axisY: {
+              title: "Units",
+              titleFontSize: 24
+            },
+           data: [{
+             type: "column",
+             dataPoints: dataPoints
+           }]
+        });
+      chart.render();
 
 
-        // Enable pusher logging
-       Pusher.logToConsole = false;
+      // Enable pusher logging
+     Pusher.logToConsole = false;
 
-       var pusher = new Pusher('e06ae81fe7ba85abcccb', {
-         cluster: 'us2',
-         encrypted: true
+     var pusher = new Pusher('e06ae81fe7ba85abcccb', {
+       cluster: 'us2',
+       encrypted: true
+     });
+
+     var channel = pusher.subscribe('movie-poll');
+
+     //we map through dataPoints and see which ID matches the data.id
+     //if the movie is in top 10, then we add the vote to dataPoints
+     channel.bind('movie-vote', function(data) {
+       dataPoints = dataPoints.map(function(x){
+         if(x.id == data.movie){
+           x.y += data.votes;
+           return x;
+         }
+         else {
+           return x;
+         }
        });
-
-       var channel = pusher.subscribe('movie-poll');
-
-       //we map through dataPoints and see which ID matches the data.id
-       //if the movie is in top 10, then we add the vote to dataPoints
-       channel.bind('movie-vote', function(data) {
-         dataPoints = dataPoints.map(function(x){
-           if(x.id == data.movie){
-             x.y += data.votes;
-             return x;
-           }
-           else {
-             return x;
-           }
-         });
-         chart.render();
-       });
-     }
+       chart.render();
+     });
+   }
   });
+
 }
+
+
 
 var dataDB = setMovies();
 
